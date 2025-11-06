37d03eee1182773307ee71f48c51be1d81e62f3c
faedhuil <Aljoschaschneider@web.de>
2025-11-06 11:40:43 +0000

    03 Prompt

---
+++ src/02_variablen.js
@@ -0,0 +1,22 @@
+let test;
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
+
+
+test = "hi"; // String
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
+
+
+test = 2; // Number
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------");
+
+test = true; // Boolean
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------");
+
