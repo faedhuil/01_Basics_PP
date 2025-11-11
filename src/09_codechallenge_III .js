c40fd4d3d60ea18ee871b0824603e3f19fb8a356
Aljoscha Schneider <Aljoschaschneider@web.de>
2025-11-11 23:53:49 +0100

    solved!

--- src/09_codechallenge_III .js
+++ src/09_codechallenge_III .js
@@ -13,4 +13,42 @@ const prompt = require('prompt-sync')({sigint: true});
 var food;
 var drink;
 
+console.log("Guten Tag bei MD, Ihre Bestellung bitte!")
 
+let auswahlFood = parseInt(prompt("Hamburger (1), Cheeseburger (2)oder Chilliburger (3) ? "));
+
+switch (auswahlFood) {
+    case 1:
+        food = "Hamburger";  
+        break;
+    case 2:
+        food = "Cheeseburger";
+        break;
+    case 3:
+        food = "Chilliburger";
+        break;
+    default:
+        break;
+}
+
+willGetraenk =  prompt("Möchten Sie ein Getränk dazu bestellen? (j/n) ");
+
+if(willGetraenk == "j" || willGetraenk == "J"){
+    let auswahlDrink = parseInt(prompt("Cola (1) , Fanta (2), Sprite (3)? "));
+    switch (auswahlDrink) {
+        case 1:
+            drink = "Cola";
+            break;
+        case 2:
+            drink = "Fanta";
+            break;
+        case 3:
+            drink = "Sprite";
+            break;
+        default:
+            break;
+    }
+    console.log("Hier ist Ihre Bestellung: " + food + " und " + drink);
+} else {
+    console.log("Hier ist Ihre Bestellung: " + food);
+} 
