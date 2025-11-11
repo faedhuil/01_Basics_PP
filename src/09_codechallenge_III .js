9824fa3c78e549e66830ed51c92bcb1d567c5ebe
faedhuil <Aljoschaschneider@web.de>
2025-11-11 11:27:09 +0000

    codechallenge 3 added

---
+++ src/09_codechallenge_III .js
@@ -0,0 +1,16 @@
+
+/***** Codechallenge III *****/
+
+// ControlFlow / Funktionen
+
+// Erstellen Sie ein JS-Programm, dass einen Bestellvorgane bei MD
+// simuliert. Orientieren Sie sich dabei am Struktogramm in der Datei
+// bestellung.json  - Datei bitte in den online-Editor STRUCTOG laden
+// https://ddi.education/struktog/
+
+const prompt = require('prompt-sync')({sigint: true});
+
+var food;
+var drink;
+
+
